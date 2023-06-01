99b3cab07a378d6abab94d86a48b7efbdd7a1162
River Cook <River.cook@kearsarge.org>
2023-06-01 08:40:20 -0400

    BER NER NER NER NAH NER

--- Alert.py
+++ Alert.py
@@ -3,9 +3,9 @@ import pygame, sys, math
 class Alert ():
     def __init__(self, startPos=[25,25], size =1):
         if size == 1:
-            self.image = pygame.image.load("Skull.png")
+            self.image = pygame.image.load("BadSkull.jpg")
         else:
-            self.image = pygame.image.load("Skull"+str(size)+".png")
+            self.image = pygame.image.load("BadSkull"+str(size)+".jpg")
         self.rect = self.image.get_rect(center = startPos)
         self.kind = "endScreen"
 
